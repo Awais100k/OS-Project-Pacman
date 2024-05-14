fb95d4adc646e40e8d2b112b8be68e7cd1312e42
Awais Khan <114641928+AwaisKhan-01@users.noreply.github.com>
2024-05-14 18:07:43 +0500

    Scoring, Lives and Ghost movement

--- pacman.cpp
+++ pacman.cpp
@@ -1,222 +1,336 @@
 #include "SFML/Graphics.hpp"
-#include "SFML/Audio.hpp"
-#include "SFML/Window.hpp"
-#include "SFML/System.hpp"
 #include <fstream>
-#include <sstream>
 #include <iostream>
+#include <vector>
 
 using namespace std;
+using namespace sf;
 
 const int mapSize = 27;
 int mapArray[mapSize][mapSize];
 float dt;
+float powerPallet = 0;
+
+int score = 0;
 
 
-//Load map Textures
-void loadMapTextures(sf::Texture *&texts,int size){
-    texts[0].loadFromFile("sprites/sky.png");
-    texts[1].loadFromFile("sprites/path.png");
-    // texts[2].loadFromFile("sprites/up_left_o.png");
-    // texts[3].loadFromFile("sprites/up_mid_o.png");
-    // texts[4].loadFromFile("sprites/up_right_o.png");
-    // texts[5].loadFromFile("sprites/mid_left_o.png");
-    // texts[6].loadFromFile("sprites/mid_right_o.png");
-    // texts[7].loadFromFile("sprites/down_left_o.png");
-    // texts[8].loadFromFile("sprites/down_mid_o.png");
-    // texts[9].loadFromFile("sprites/down_right_o.png");
-    // texts[10].loadFromFile("sprites/up_left.png");
-    // texts[11].loadFromFile("sprites/up_mid.png");
-    // texts[12].loadFromFile("sprites/up_right.png");
-    // texts[13].loadFromFile("sprites/mid_left.png");
-    // texts[14].loadFromFile("sprites/mid_right.png");
-    // texts[15].loadFromFile("sprites/down_left.png");
-    // texts[16].loadFromFile("sprites/down_mid.png");
-    // texts[17].loadFromFile("sprites/down_right.png");
+// Load map textures
+void loadMapTextures(sf::Texture *&texts, int size) {
+    texts[0].loadFromFile("sprites/map/0.png");
+    texts[1].loadFromFile("sprites/map/1.png");
+    texts[14].loadFromFile("sprites/map/14.png");
+    texts[15].loadFromFile("sprites/map/15.png");
+}
+
+bool canMove(int x, int y) {
+    // if (x < 0 || x >= mapSize || y < 0 || y >= mapSize)
+    //     return false;
+    return (mapArray[y][x] == 1) || (mapArray[y][x] == 14) || (mapArray[y][x] == 15);
+}
+void CollectPallets(int x , int y){
+    if(mapArray[y][x] == 14){
+        score++;
+        mapArray[y][x] = 1;
+    }
+}
+
+void updateGhosts(vector<Sprite> &ghostSprites, vector<int> &ghostDirections, 
+                  vector<Texture> &upTextures, vector<Texture> &downTextures, 
+                  vector<Texture> &leftTextures, vector<Texture> &rightTextures, float dt) {
+    static float elapsed_time = 0.0f;
+    elapsed_time += dt;
+
+    if (elapsed_time >= 0.2f) { 
+        elapsed_time = 0.0f; 
+
+        for (size_t i = 0; i < ghostSprites.size(); ++i) {
+            int newX = ghostSprites[i].getPosition().x / 16;
+            int newY = ghostSprites[i].getPosition().y / 16;
+
+            int nextX = newX;
+            int nextY = newY;
+
+            switch (ghostDirections[i]) {
+                case 0: // up
+                    nextY--;
+                    break;
+                case 1: // down
+                    nextY++;
+                    break;
+                case 2: // left
+                    nextX--;
+                    break;
+                case 3: // right
+                    nextX++;
+                    break;
+            }
+
+            if (canMove(nextX, nextY)) {
+                ghostSprites[i].setPosition(nextX * 16, nextY * 16);
+                newX = nextX;
+                newY = nextY;
+            } else {
+                vector<int> validDirections;
+                for (int j = 0; j < 4; j++) {
+                    int dX = (j == 2) ? -1 : (j == 3) ? 1 : 0;
+                    int dY = (j == 0) ? -1 : (j == 1) ? 1 : 0;
+                    if (canMove(newX + dX, newY + dY)) {
+                        validDirections.push_back(j);
+                    }
+                }
+                if (!validDirections.empty()) {
+                    ghostDirections[i] = validDirections[rand() % validDirections.size()];
+                }
+            }
+
+            // Update ghost's texture based on its direction
+            switch (ghostDirections[i]) {
+                case 0:
+                    ghostSprites[i].setTexture(upTextures[i]);
+                    break;
+                case 1:
+                    ghostSprites[i].setTexture(downTextures[i]);
+                    break;
+                case 2:
+                    ghostSprites[i].setTexture(leftTextures[i]);
+                    break;
+                case 3:
+                    ghostSprites[i].setTexture(rightTextures[i]);
+                    break;
+            }
+        }
+    }
+}
+
+
+// void movePlayer(Sprite &playerSprite, Keyboard::Key direction) {
+//     int newX = playerSprite.getPosition().x / 16;
+//     int newY = playerSprite.getPosition().y / 16;
+//     switch (direction) {
+//         case Keyboard::Up:
+//             if (canMove(newX, newY - 1)){
+//                 playerSprite.move(0, -16);
+//                 CollectPallets(newX , newY - 1);
+//             }
+//             break;
+//         case Keyboard::Down:
+//             if (canMove(newX, newY + 1)){
+//                 playerSprite.move(0, 16);
+//                 CollectPallets(newX , newY + 1);
+//             }
+//             break;
+//         case Keyboard::Left:
+//             if (newX <= 0){
+//                 playerSprite.move(26*16,0);
+//                 CollectPallets(26 , newY);
+//             }
+//             else if (canMove(newX - 1, newY)){
+//                 playerSprite.move(-16, 0);
+//                 CollectPallets(newX - 1, newY);
+//             }
+//             break;
+//         case Keyboard::Right:
+//             if (newX >= 26){
+//                 playerSprite.move(-26*16,0);
+//                 CollectPallets(0, newY);
+//             }
+//             else if (canMove(newX + 1, newY)){
+//                 playerSprite.move(16, 0);
+//                 CollectPallets(newX + 1, newY);
+//             }
+//             break;
+//     }
+// }
+
+void movePlayer(Sprite &playerSprite, Keyboard::Key keyPressed) {
+     int newX = playerSprite.getPosition().x / 16;
+    int newY = playerSprite.getPosition().y / 16;
+    switch (keyPressed) {
+        case Keyboard::Up:
+            newY--;
+            break;
+        case Keyboard::Down:
+            newY++;
+            break;
+        case Keyboard::Left:
+            newX--;
+            break;
+        case Keyboard::Right:
+            newX++;
+            break;
+    }
+    if (canMove(newX, newY)) {
+        
+        playerSprite.setPosition(newX * 16, newY * 16);
+        CollectPallets(newX, newY);
+    } else {
+        if((playerSprite.getPosition().x / 16) == 26 && (newX == 27)){
+                playerSprite.setPosition(0, newY * 16);
+        }
+        else if((playerSprite.getPosition().x / 16) == 0 && (newX == -1)){
+            playerSprite.setPosition(26 * 16,newY * 16);
+        }
+    }
+    cout<<newX << " "<<newY<<endl;
+    cout<<"Position : "<<playerSprite.getPosition().x / 16 <<" "<<playerSprite.getPosition().y /16<<endl;
 }
 
 
 
 
+int main() {
+    sf::RenderWindow window(sf::VideoMode(576, 800), "Pacman");
+    window.setFramerateLimit(120);
 
-int main(){
-   
-
-    //SFML VARIABLES
-    sf::RenderWindow* window;
-	sf::Event sfEvent;
-	sf::Clock dtClock;
-    
-    //Init Window
-    window = new sf::RenderWindow(sf::VideoMode(576,576), "Pacman");
-    window->setFramerateLimit(120);
-
-    //Map textures;
-    int noTextures = 2;
+    // Map textures
+    int noTextures = 18;
     sf::Texture *mapText = new sf::Texture[noTextures];
-    loadMapTextures(mapText,noTextures);
-    
+    loadMapTextures(mapText, noTextures);
 
-
-    sf::Sprite *mapSprites = new sf::Sprite[noTextures];    
-    for (int i = 0; i < noTextures; i++)
-    {
+    sf::Sprite *mapSprites = new sf::Sprite[noTextures];
+    for (int i = 0; i < noTextures; i++) {
         mapSprites[i].setTexture(mapText[i]);
     }
-    
 
-    //Loading the map from the text file
     std::ifstream ifs("maps/map1.txt");
     if (ifs.is_open()) {
-        //int mapArray[mapSize][mapSize];
-        char a, b; // Use two characters to read the number
+        char a, b;
         for (int i = 0; i < mapSize; i++) {
             for (int j = 0; j < mapSize; j++) {
-                ifs.get(a); // Read the first digit
-                if(a == '\n' ||  a == ' ')
+                ifs.get(a);
+                if (a == '\n' || a == ' ')
                     j--;
-                // else if (b == ' ') // If there's a space after the second digit
-                //     mapArray[i][j] = (a - '0') * 10; // Convert first digit to integer
                 else {
-                    ifs.get(b); // Read the second digit
-                    mapArray[i][j] = (a - '0') * 10 + (b - '0'); // Convert both digits to integer
+                    ifs.get(b);
+                    mapArray[i][j] = (a - '0') * 10 + (b - '0');
                 }
             }
         }
     }
-    
-    
-    
-    
-    
-    
-    
-    
     ifs.close();
-    for (int i = 0; i < mapSize; i++)
-        {
-            for (int j = 0; j < mapSize; j++)
-            {
-                //mapArray[i][j] = 2;
-                cout<<mapArray[i][j]<<" ";
-            }
-            cout<<endl;
+
+    // Print map array
+    for (int i = 0; i < mapSize; i++) {
+        for (int j = 0; j < mapSize; j++) {
+            if(mapArray[i][j] < 10)
+                cout <<"0"<< mapArray[i][j] << " ";
+            else
+                cout << mapArray[i][j] << " ";
         }
-    //Main Loop
+        cout << endl;
+    }
 
-    while (window->isOpen()) {
+    // Create ghosts
+    const int numGhosts = 4;
+    vector<Sprite> ghostSprites(numGhosts);
+    vector<Texture> upTextures(numGhosts), downTextures(numGhosts), 
+                    leftTextures(numGhosts), rightTextures(numGhosts);
+    vector<int> ghostDirections(numGhosts, 0); // 0: up, 1: down, 2: left, 3: right
+
+    for (int i = 0; i < numGhosts; ++i) {
+        upTextures[i].loadFromFile("sprites/str.png");
+        downTextures[i].loadFromFile("sprites/str.png");
+        leftTextures[i].loadFromFile("sprites/str.png");
+        rightTextures[i].loadFromFile("sprites/str.png");
+        ghostSprites[i].setTexture(upTextures[i]);
+    }
+
+    ghostSprites[0].setPosition(((mapSize/2) + 1 )* 16, ((mapSize/2) + 1)* 16);
+    ghostSprites[1].setPosition(100 , 400);
+    ghostSprites[2].setPosition(100 , 99);
+    ghostSprites[3].setPosition(100 , 99);
 
 
-        ////////////////////////////////Update//////////////////////////////////////////
+    // Player (Pacman) setup
+    Texture pacmanTexture;
+    pacmanTexture.loadFromFile("sprites/player.png");
+    Sprite pacmanSprite;
+    pacmanSprite.setTexture(pacmanTexture);
+    pacmanSprite.setPosition(400, 100);
+    int lives = 3;
+
+
+    Font font;
+    font.loadFromFile("sprites/Arial.ttf");
+    Text livesText;
+    Text scoreText;
+    livesText.setFont(font);
+    livesText.setCharacterSize(24);
+    livesText.setFillColor(Color::White);
+    livesText.setPosition(450, 10);
+
+    scoreText.setFont(font);
+    scoreText.setCharacterSize(24);
+    scoreText.setFillColor(Color::White);
+    scoreText.setPosition(450, 40);
+
+
+    // Main loop
+    sf::Clock dtClock;
+    while (window.isOpen() && lives>=0) {
+        //cout<<"x:"<<pacmanSprite.getPosition().x<<"  y:"<<pacmanSprite.getPosition().y;
 
         dt = dtClock.restart().asSeconds();
-        while (window->pollEvent(sfEvent)) {
+
+        sf::Event sfEvent;
+        while (window.pollEvent(sfEvent)) {
             if (sfEvent.type == sf::Event::Closed)
-                window->close();
-            if(sfEvent.type == sf::Event::KeyPressed){
-                if(sfEvent.key.code == sf::Keyboard::Escape);
-                    window->close();
-            }
-        }  
+                window.close();
+            if (sfEvent.type == sf::Event::KeyPressed) {
+                if (sfEvent.key.code == sf::Keyboard::Escape)
+                    window.close();
 
+                movePlayer(pacmanSprite, sfEvent.key.code);
+            }
+        }
+
+        // Update ghosts
+        updateGhosts(ghostSprites, ghostDirections, upTextures, downTextures, leftTextures, rightTextures, dt);
         
-
-
-
-        //////////////////////////////Render///////////////////////////////////////////
-
-        window->clear();
-
-        for(int i = 0 ; i < mapSize ; i++){
-            for(int j = 0 ; j < mapSize ; j++){
-                if(mapArray[i][j] == 0){
-                    mapSprites[0].setPosition(j*16,i*16);
-                    window->draw(mapSprites[0]);
-                }
-                else if(mapArray[i][j] == 1){
-                    mapSprites[1].setPosition(j*16,i*16);
-                    window->draw(mapSprites[1]);
-                }
-                // else if(mapArray[i][j] == 2){
-                //     mapSprites[2].setPosition(j*16,i*16);
-                //     window->draw(mapSprites[2]);
-                // }
-                // else if(mapArray[i][j] == 3){
-                //     mapSprites[3].setPosition(j*16,i*16);
-                //     window->draw(mapSprites[3]);
-                // }
-                // else if(mapArray[i][j] == 4){
-                //     mapSprites[4].setPosition(j*16,i*16);
-                //     window->draw(mapSprites[4]);
-                // }
-                // else if(mapArray[i][j] == 5){
-                //     mapSprites[5].setPosition(j*16,i*16);
-                //     window->draw(mapSprites[5]);
-                // }
-                // else if(mapArray[i][j] == 6){
-                //     mapSprites[6].setPosition(j*16,i*16);
-                //     window->draw(mapSprites[6]);
-                // }
-                // else if(mapArray[i][j] == 7){
-                //     mapSprites[7].setPosition(j*16,i*16);
-                //     window->draw(mapSprites[7]);
-                // }
-                // else if(mapArray[i][j] == 8){
-                //     mapSprites[8].setPosition(j*16,i*16);
-                //     window->draw(mapSprites[8]);
-                // }
-                // else if(mapArray[i][j] == 9){
-                //     mapSprites[9].setPosition(j*16,i*16);
-                //     window->draw(mapSprites[9]);
-                // }
-                // else if(mapArray[i][j] == 10){
-                //     mapSprites[10].setPosition(j*16,i*16);
-                //     window->draw(mapSprites[10]);
-                // }
-                // else if(mapArray[i][j] == 11){
-                //     mapSprites[11].setPosition(j*16,i*16);
-                //     window->draw(mapSprites[11]);
-                // }
-                // else if(mapArray[i][j] == 12){
-                //     mapSprites[12].setPosition(j*16,i*16);
-                //     window->draw(mapSprites[12]);
-                // }
-                // else if(mapArray[i][j] == 13){
-                //     mapSprites[13].setPosition(j*16,i*16);
-                //     window->draw(mapSprites[13]);
-                // }
-                // else if(mapArray[i][j] == 14){
-                //     mapSprites[14].setPosition(j*16,i*16);
-                //     window->draw(mapSprites[14]);
-                // }
-                // else if(mapArray[i][j] == 15){
-                //     mapSprites[15].setPosition(j*16,i*16);
-                //     window->draw(mapSprites[15]);
-                // }
-                // else if(mapArray[i][j] == 16){
-                //     mapSprites[16].setPosition(j*16,i*16);
-                //     window->draw(mapSprites[16]);
-                // }
-                // else if(mapArray[i][j] == 17){
-                //     mapSprites[17].setPosition(j*16,i*16);
-                //     window->draw(mapSprites[17]);
-                // }
-
-
-        //cout<<map.grid[i][j][0].id<<endl;
+        // Check collisions with ghosts
+        for (size_t i = 0; i < ghostSprites.size(); ++i) {
+            if (pacmanSprite.getGlobalBounds().intersects(ghostSprites[i].getGlobalBounds())) {
+                lives--;
+                pacmanSprite.setPosition(400, 100);
             }
-        } 
-
-
-        //window->draw(circle);
+        }
 
 
 
+        /////////////////////////////////Render////////////////////////////
 
+        window.clear();
 
-        window->display();
+        for (int i = 0; i < mapSize; i++) {
+            for (int j = 0; j < mapSize; j++) {
+                if(mapArray[i][j] == 14){
+                    mapSprites[1].setPosition(j*16,i*16);
+                    window.draw(mapSprites[1]);
+                }
+                else if(mapArray[i][j] == 15){
+                    mapSprites[1].setPosition(j*16,i*16);
+                    window.draw(mapSprites[1]);
+                }
+                mapSprites[mapArray[i][j]].setPosition(j*16,i*16);
+                window.draw(mapSprites[mapArray[i][j]]);
+            }
+        }
 
+        window.draw(pacmanSprite);
+
+        for (int i = 0; i < numGhosts; ++i) {
+            window.draw(ghostSprites[i]);
+        }
+
+        // Draw lives
+        
+        livesText.setString("Lives: " + to_string(lives));
+        scoreText.setString("Score: " + to_string(score));
+        window.draw(livesText);
+        window.draw(scoreText);
+
+        window.display();
     }
 
-
+    return 0;
 }
